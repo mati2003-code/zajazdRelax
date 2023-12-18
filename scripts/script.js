c712b208dc5b221b1196fcb854f2dbccda4d3f44
mati2003-code <mateusz.dziobakop.pl@op.pl>
2023-12-18 23:27:15 +0100

    add slip menu

--- scripts/script.js
+++ scripts/script.js
@@ -5,24 +5,12 @@ const hamburgerMenuButton = document.querySelector(".fa-solid");
 
 const menuSettings = () => {
   hamburgerMenuButton.addEventListener("click", () => {
-    siteMenu.classList.add("animation-appear");
-    siteMenu.classList.toggle("display-block");
+    siteMenu.classList.toggle("active");
   });
 
   siteMenu.addEventListener("click", () => {
-    siteMenu.classList.remove("display-block");
-  });
 
-  window.addEventListener("resize", () => {
-    if (window.innerWidth >= 768) {
-      siteMenu.classList.add("animation-disappear");
-
-      setTimeout(() => {
-        siteMenu.classList.remove("display-block");
-      }, 500);     
-    } else {
-      siteMenu.classList.remove("animation-disappear");
-    }
+    siteMenu.classList.remove("active");
   });
 }
 
